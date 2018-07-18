cffd6b951714aed192a812b13c6475c58da214c3
Don Pho <donpho@Dons-MacBook-Air.local>
2018-07-18 11:03:22 -0500

    npm and module exercise

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const $ = require('jquery');
 
-const sayHello = (name) => console.log('Hello, ' + name + '!');
+// const sayHello = (name) => console.log('Hello, ' + name + '!');
+//
+// sayHello('Don');
 
-sayHello('Don');
+const message = require('./say-hello');
+
+console.log(message());
